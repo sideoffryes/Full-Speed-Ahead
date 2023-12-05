d1fe53ad95e0187fd95bdc7def3f650f292535eb
sideoffryes <m251854@usna.edu>
2023-12-05 16:27:09 -0500

    updated some files to new style

--- createSearch.php
+++ createSearch.php
@@ -1,142 +1,152 @@
 <!-- Script creates a table of workouts that match the filter reuiqrements set by the user when they filled out the form on serch.html
     TODOS: none that I can think of-->
 
-<title>Proj Create Search</title>
-<head>
-<meta charset="utf-8">
-    <title>Proj Sign confirm</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="toolbar.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-
-  <style type="text/css">
-    
-    th{
-        background-color: #ccdbe6;
-    }
-  </style>
-</head>
-
-<body>
-    <!-- nav bar-->
-    <div class="topnav">
-        <a  href="index.html">Home</a>
-        <a class="active" href="search.php">Search Workouts</a>
-        <a  href="logWork.php">Log Workouts</a>  
-        <a  href="login.html">Login</a>
-        <a  href="signup.html">Sign up</a>
-        <a href = "logout.php"> Log out</a>
-    </div> <br><br>
-
-<h2 style = "text-align: center; color: #051026"> Search Results </h2>
-  <table class = "table" style="margin:auto; width: 1500px;">
-    
-    <!-- start table -->
-    <thead>
-      <tr><th scope = "col">FIRST NAME</th><th scope = "col">LAST NAME</th><th scope = "col">ALPHA</th><th scope = "col">COMPANY</th><th scope = "col">CLASS YEAR</th>
-          <th scope = "col">TYPE</th><th scope = "col">DURATION IN MIN</th><th scope = "col">LOCATION</th><th scope = "col">DISTANCE IN MILES</th></tr>
-  </thead>
-  
-
-  <?php
-    //check if type value was selected by user. if not, set it to an empty string.
-    $type = $_POST['type'];
-    if($type == "Select Type"){
-        $type = "";
-    }
-    //get values of other filter fields
-    $company = $_POST['company'];
-    $alpha = $_POST['alpha'];
-
-    //open LOGWORK.txt. This is the file where lopWork.php saves its form entries
-    $fp = fopen('LOGWORK.txt', 'r');
-    $reportArray= [];
-    $tab = '/t';
-    $count = 0;
-    $found = 1;
-    
-
-    if (!$fp) {                   
-      echo "<p>ERROR! Could not open file LOG.txt for reading.</p>";
-    } 
-    else 
-    {
-      $line = fgets($fp);          // read lines
-      while( !feof($fp) ) 
-      {
-
-        //check if form variables are empty. If they are, ignore them.
-        if($type !== ""){
-            if(!str_contains($line, $type)){
-                $found = 0;
-            }
-
-        }
-        if($company !== ""){
-            if(!str_contains($line, $company)){
-                $found = 0;
-            }
-
-        }
-        if($alpha !== ""){
-            if(!str_contains($line, $alpha)){
-                $found = 0;
-            }
-
-        }
-
-        //if line meets filter requirements, save to be printed later
-        if($found == 1){
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+            
+        <title>Search Results</title>
         
-          $parsedRow = explode("\t", $line);
-          $reportArray[$count] = $parsedRow;
-          $count += 1;
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+    </head>
 
-          }
-          $line = fgets($fp);
-         $found = 1;
+  <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-sm bg-dark" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Full Speed Ahead</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                            
+                        <!-- Workouts drop down menu -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Workouts</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="search.php">Search Workouts</a></li>
+                                <li class="nav-item"><a class="nav-link" href="logWork.php">Log Workouts</a></li>
+                            </ul>
+                        </li>
 
-      }
-          
-        
-    }
+                        <!-- Link to admin page -->
+                        <li class="nav-item"><a class="nav-link" href="admin.html">Admin Page</a></li>
 
+                        <!-- Account drop down menu -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="signup.html">Sign Up</a></li>
+                                <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+                                <li class="nav-item"><a class="nav-link" href="logout.php">Log Out</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
+        <!-- Page title -->
+        <div class="container-fluid bg-dark-subtle">
+            <h1 class="display-4 fw-bold text-center">Search Results</h1>
+            <p class="fs-4 text-center">Table is sorted in ascending order by user's class year.</p>
+        </div>
 
-    fclose($fp);  
+        <div class="container-fluid">
+            <table class = "table table-striped table-hover table-sm text-center">
+            
+                <!-- start table -->
+                <thead>
+                    <tr>
+                        <th scope = "col">FIRST NAME</th>
+                        <th scope = "col">LAST NAME</th>
+                        <th scope = "col">ALPHA</th>
+                        <th scope = "col">COMPANY</th>
+                        <th scope = "col">CLASS YEAR</th>
+                        <th scope = "col">TYPE</th>
+                        <th scope = "col">DURATION IN MIN</th>
+                        <th scope = "col">LOCATION</th>
+                        <th scope = "col">DISTANCE IN MILES</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <?php
+                    //check if type value was selected by user. if not, set it to an empty string.
+                    $type = $_POST['type'];
+                    if($type == "Choose a workout type"){
+                        $type = "";
+                    }
+                    //get values of other filter fields
+                    $company = $_POST['company'];
+                    $alpha = $_POST['alpha'];
 
-    //sort and print results
-    array_multisort((array_column($reportArray, 2)), SORT_ASC, $reportArray);  
+                    //open LOGWORK.txt. This is the file where lopWork.php saves its form entries
+                    $fp = fopen('LOGWORK.txt', 'r');
+                    $reportArray= [];
+                    $tab = '/t';
+                    $count = 0;
+                    $found = 1;
+                    
 
-    $k = 0;
-    while($k<$count)
-    {
-      echo "<tr>";
-      for($i= 0; $i <9; $i++){
-        echo "<td>";
-        echo $reportArray[$k][$i];
-        echo "</td>";  
-      }
-      echo "</tr>";
-      
-      $k++;
-    }
+                    if (!$fp) {                   
+                        echo "<p>ERROR! Could not open file LOG.txt for reading.</p>";
+                    } else {
+                        $line = fgets($fp);          // read lines
+                        while( !feof($fp) ) {
+                        //check if form variables are empty. If they are, ignore them.
+                        if($type !== ""){
+                            if(!str_contains($line, $type)){
+                                $found = 0;
+                            }
+                        }
+                        if($company !== ""){
+                            if(!str_contains($line, $company)){
+                                $found = 0;
+                            }
+                        }
+                        if($alpha !== ""){
+                            if(!str_contains($line, $alpha)){
+                                $found = 0;
+                            }
+                        }
+                        //if line meets filter requirements, save to be printed later
+                        if($found == 1){
+                            $parsedRow = explode("\t", $line);
+                            $reportArray[$count] = $parsedRow;
+                            $count += 1;
+                        }
+                        $line = fgets($fp);
+                        $found = 1;
+                        }
+                    }
 
-    ?>
-   
+                    fclose($fp);  
 
+                    //sort and print results
+                    array_multisort((array_column($reportArray, 2)), SORT_ASC, $reportArray);  
 
+                    $k = 0;
+                    while($k<$count) {
+                        echo "<tr>";
+                        for($i= 0; $i <9; $i++){
+                        echo "<td>";
+                        echo $reportArray[$k][$i];
+                        echo "</td>";  
+                        }
+                        echo "</tr>";
+                        
+                        $k++;
+                    } ?>
+                </tbody>    
+            </table>
+        </div>
 
-  <p style="text-align:center;"> Table is sorted in ascending order by user's class year.<p>
+        <!-- button to return to search page -->
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <a href="search.php" class="btn btn-secondary btn-lg" role="button" aria-pressed="false">New Search</a>
+        </div>
 
-  </table>
-  <br><br>
-
-<!-- button to return to search page -->
-<div class="d-flex justify-content-center">
-  <a href="search.php" class="btn btn-dark btn-lg active" role="button" aria-pressed="true" style="text-align:center">New Search</a>
-</div>
-</body>
+    </body>
+</html>
--- search.php
+++ search.php
@@ -2,109 +2,111 @@
 The choices for filters are by class year, alpha, or company.
 TODO: none -->
 <!DOCTYPE html>
-<?php
-  session_start();
-?>
+<?php session_start(); ?>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <title>Proj Log</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="toolbar.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-
-
-
-</head>
-
-  
-<?php
-  if (!isset($_SESSION['username'])) {
-    ?><br><br><br> 
-    <!-- error message for if user is not logged in. Gices user link back to login page -->
-     <div class="card" style="width: 50rem; margin: auto; margin-top: 100px color:051026;">
-        <div class="card-body" style="color:051026;">
-            <h4 class="card-title"style="text-align:center;">Error</h4>
-            <p class="card-text" style="text-align:center;">Please login to search workouts.</p>
-            <div style="text-align: center;">
-                <a href="login.html" class="card-link">Login Page</a>
-             </div>
-        </div>
-        </div>
-<?php
-  }
-  ///if user is logged in
-  else{?>
-    <body>
-    <!-- nav bar -->
-    <div class="topnav">
-        <a  href="index.html">Home</a>
-        <a class="active" href="search.php">Search Workouts</a>
-        <a  href="logWork.php">Log Workouts</a>  
-        <a  href="login.html">Login</a>
-        <a  href="signup.html">Sign up</a>
-        <a href="admin.html">Admin</a>
-        <a href = "logout.php"> Log out</a>
-
-    </div>
-
-    <div class="jumbotron" style="color: rgba(251, 213, 130, 0.789); background-color:#051026; ">
-        <h1 class="display-4" style="text-align: center;">Search workouts</h1>
-       
-        <p style="text-align: center;">Fill out the form below to search for workouts logged by users. Fill out the fields for desired filters.</p>
-      
-       
-        
-      </div>
-
-      <br>
-        <h1 style="color:#051026; text-align: center;">Search Filters</h1>
-        <br>
-        <form class = "borders"  method="post" action="createSearch.php">
-
-
-            <div class="form-outline mb-4">
-                <input type="text" id="alpha" name = "alpha" class="form-control" />
-                <label class="form-label" for="location">Alpha</label>
-            </div> <br>
-
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
             
-            <div class="form-outline mb-4">
-                <input type="text" id="company" name="company" class="form-control" />
-                <label class="form-label" for="Company">Company</label>
-            </div> <br>
-
-    
-            <div class="form-outline mb-4">
-            <div id ="sel">
-                <select  id="type" name = "type" class="form-select" >
-                    <option selected>Select Type</option>
-                    <option value="run">Run</option>
-                    <option value="swim">Swim</option>
-                    <option value="bike">Bike</option>
-                    <option value="walk">Walk</option>
-                    
-                </select>
-             </div>
-             <label for="sel">Type of Workout</label> 
-            </div> <br>
-
+        <title>New Search</title>
         
-    
-        
-            <!-- calls  -->
-            <button onclick=refresh() class="btn btn-primary btn-block mb-4">Search</button>
-  </form>
-  <div id="content">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> 
+    </head>
 
   
+    <?php
+    if (!isset($_SESSION['username'])) {
+        ?><br><br><br> 
+        <!-- error message for if user is not logged in. Gices user link back to login page -->
+        <div class="card" style="width: 50rem; margin: auto; margin-top: 100px color:051026;">
+            <div class="card-body" style="color:051026;">
+                <h4 class="card-title"style="text-align:center;">Error</h4>
+                <p class="card-text" style="text-align:center;">Please login to search workouts.</p>
+                <div style="text-align: center;">
+                    <a href="login.html" class="card-link">Login Page</a>
+                </div>
+            </div>
+        </div>
+    <?php
+    }else{?>
+    <!-- if user is logged in -->
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-sm bg-dark" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Full Speed Ahead</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                            
+                        <!-- Workouts drop down menu -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Workouts</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link active" aria-current="page" href="search.php">Search Workouts</a></li>
+                                <li class="nav-item"><a class="nav-link" href="logWork.php">Log Workouts</a></li>
+                            </ul>
+                        </li>
 
+                        <!-- Link to admin page -->
+                        <li class="nav-item"><a class="nav-link" href="admin.html">Admin Page</a></li>
 
-  </body>
+                        <!-- Account drop down menu -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="signup.html">Sign Up</a></li>
+                                <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
+                                <li class="nav-item"><a class="nav-link" href="logout.php">Log Out</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
+        <!-- Page title -->
+        <div class="container-fluid bg-dark-subtle">
+                <h1 class="display-4 fw-bold text-center">Search Workouts</h1>
+                <p class="fs-4 text-center">Fill out the form below to search for workouts logged by users. Fill out the fields for desired filters.</p>
+        </div>
+        
+        <div class="container-fluid">        
+            <h2 class="display-5 fw-bold text-center">Search Filters</h1>
+
+            <!-- new bootstrap form -->
+            <form method="post" action="createSearch.php">
+                <!-- Alpha input field -->
+                <div class="mb-3">
+                    <label for="alpha" class="form-label">Alpha</label>
+                    <input type="text" class="form-control" id="alpha" aria-describedby="alphaHelp" name="alpha">
+                    <div id="alphaHelp" class="form-text">Enter your Midshipman Alpha number here.</div>
+                </div>
+                
+                <!-- Company input field -->
+                <div class="mb-3">
+                    <label for="company" class="form-label">Company</label>
+                    <input type="text" class="form-control" id="company" aria-describedby="companyHelp" name="company">
+                    <div id="companyHelp" class="form-text">Enter your company number here.</div>
+                </div>
+                
+                <!-- Workout select field -->
+                <div class="mb-3">
+                    <label for="workout" class="form-label">Workout Type</label>
+                    <select class="form-select" aria-label="Workout selection" id="type" name="type">
+                        <option selected>Choose a workout type</option>
+                        <option value="1">Run</option>
+                        <option value="2">Swim</option>
+                        <option value="3">Bike</option>
+                        <option value="4">Walk</option>
+                    </select>
+                    <div id="workoutHelp" class="form-text">Enter the type of workout you completed here.</div>
+                </div>
+                <button onclick=refresh() class="btn btn-primary btn-block mb-4">Search</button>
+            </form>
+        </div>
+    </body>
 
  <?php }?>
  <script type="text/javascript">
